afa81b1524cd7725a8e5516de3ac3764d6ca6de5
monishka8303 <115769741+Monishka8303@users.noreply.github.com>
2023-02-23 17:47:43 +0530

    Add files via upload

---
+++ Linked List/InsertAtEndInDLL.java
@@ -0,0 +1,40 @@
+class Node{
+    int data;
+    Node prev;
+    Node next;
+    Node(int data){
+        this.data=data;
+        prev=null;
+        next=prev;
+    }
+}
+public class InsertAtEndInDLL {
+    static Node insertEnd(Node head,int data){
+        Node temp=new Node(data);
+        if(head==null){
+            return temp;
+        }
+        Node curr=head;
+        while(curr.next!=null){
+            curr=curr.next;
+        }
+        curr.next=temp;
+        temp.prev=curr;
+        return head;
+    }
+    static void printList(Node head){
+        Node curr=head;
+        while(curr!=null){
+            System.out.print(curr.data+" ");
+            curr=curr.next;
+        }
+    }
+    public static void main(String[] args) {
+        Node head=null;
+        head=insertEnd(head,40);
+        head=insertEnd(head,30);
+        head=insertEnd(head,20);
+        head=insertEnd(head,10);
+        printList(head);
+    }
+}
